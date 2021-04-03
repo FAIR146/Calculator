b352e05a4fedabaf4e99f9fbdf8b0ba3d191ae7e
Никита Митраченков <nickitaipad@gmail.com>
2021-04-03 15:23:04 +0300

    Repos initialize

---
+++ CalculatorX2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorX2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+
+            int div = number1 * number2 * number3 * 2;
+
+            Console.WriteLine(+div);
+
+            Console.ReadKey();
+        }
+    }
+}
